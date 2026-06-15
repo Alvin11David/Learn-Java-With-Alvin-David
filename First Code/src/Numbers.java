83b4d77761c3c3178e592b377b6a0e61d5ffd691
Kainamatsikofaith <faithkainamatsiko@gmail.com>
2026-06-15 13:06:56 +0300

    Learnt How To Print Numbers Using The Println() Method

---
+++ First Code/src/Numbers.java
@@ -0,0 +1,19 @@
+public class Numbers {
+    public static void main(String[] args) {
+        System.out.println("3"); //If you place a number in double quotes, it can still be printed out
+        System.out.println(4); //This is the better way of printing numbers
+
+        //Performing mathematical calculations using the println() method
+        //Addition
+        System.out.println(3+4);
+
+        //Subtraction
+        System.out.println(5-8);
+
+        //Multiplication
+        System.out.println(8*2);
+
+        //Division
+        System.out.println(8/4);
+    }
+}
