66ae5ff4ca4e43b73a05e995f04940416295705b
Kainamatsikofaith <faithkainamatsiko@gmail.com>
2026-06-15 13:30:58 +0300

    Made the second java code challenge

---
+++ First Code/src/NameAge.java
@@ -0,0 +1,16 @@
+/*
+Challenge: Print Name and Age
+Test your understanding of Java Output by completing a small coding challenge.
+
+Instructions
+Inside main(), complete the following steps:
+Use print() or println() to print your name
+Use another print() or println() to print your age
+* */
+
+public class NameAge {
+    public static void main(String[] args) {
+        System.out.println("Alvin David"); //Printing the name using println() method
+        System.out.print(25); //Printing the age using print() method
+    }
+}
