7742d6bcc51b36680af1827da4fe4b897cb9ec14
Kainamatsikofaith <faithkainamatsiko@gmail.com>
2026-06-15 14:01:23 +0300

    Learnt about Java Comment Types

---
+++ First Code/src/Comments.java
@@ -0,0 +1,14 @@
+public class Comments {
+    public static void main(String[] args) {
+        // Using a single line comment
+        System.out.println("Hello"); //This statement prints out Hello
+        System.out.println("Viewers");//This statement prints out Viewers//
+
+        // Using the multi line comment
+
+        /* This statement performs a mathematical addition calculation
+        of two Numbers
+         */
+        System.out.println(3+4);
+    }
+}
