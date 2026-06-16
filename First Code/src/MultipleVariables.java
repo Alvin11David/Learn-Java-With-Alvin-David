a5a81a2908a661a34bbaddd9ae6f0269eeb85a27
Kainamatsikofaith <faithkainamatsiko@gmail.com>
2026-06-16 23:14:42 +0300

    Learnt about Multiple Variables

---
+++ First Code/src/MultipleVariables.java
@@ -0,0 +1,15 @@
+public class MultipleVariables {
+    public static void main(String[] args) {
+        int x = 10, y = 20, z = 30;
+        System.out.println(x + y + z);
+
+        //float
+        float x1 = 5.5f, y1 = 6.6f, z1 = 7.7f;
+        System.out.println(x1+y1+z1);
+
+        //Assigning the same value to all variables
+        int x2, y2, z2;
+        x2 = y2 = z2 = 70;
+        System.out.println(x2+y2+z2);
+    }
+}
