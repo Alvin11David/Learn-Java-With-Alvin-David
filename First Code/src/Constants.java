217ef4ada0b4de5b5771ed601a9305da930109e7
Kainamatsikofaith <faithkainamatsiko@gmail.com>
2026-06-17 10:58:58 +0300

    Learnt about constants

---
+++ First Code/src/Constants.java
@@ -0,0 +1,5 @@
+public class Constants {
+    public static void main(String[] args) {
+        final int  AGE = 26; //This is my current age and it can not change till next year on my birthday
+    }
+}
