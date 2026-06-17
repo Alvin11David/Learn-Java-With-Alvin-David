2f34478c8f48c056b8435e26667a90d2ab18d47b
Kainamatsikofaith <faithkainamatsiko@gmail.com>
2026-06-17 11:56:56 +0300

    Made the java code challenge three

---
+++ First Code/src/JavaCodeChallengeThree.java
@@ -0,0 +1,18 @@
+/*Instructions
+Inside main(), complete the following steps:
+Create a String variable named name and set it to your name
+Create an int variable named age and set it to your age
+Print name
+Print age
+Note: Remember semicolons.
+
+ */
+public class JavaCodeChallengeThree {
+    public static void main(String[] args) {
+        String name = "Alvin David";
+        int age = 25;
+
+        System.out.println(name);
+        System.out.println(age);
+    }
+}
